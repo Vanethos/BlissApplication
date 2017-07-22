4c0779a307dcf1e00e1b7a3a26e9866915c7a71d
Vanethos <solid.goncalo@gmail.com>
2017-07-22 21:13:02 +0100

    Feature: Check Internet Connectivity - New Class Added: ConnectivityStatus - When there is no connectivity, a new fragment appears and we cannot  continue on the app

--- app/src/main/java/com/sardinecorp/blissapplication/MainActivity.java
+++ app/src/main/java/com/sardinecorp/blissapplication/MainActivity.java
@@ -5,11 +5,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
+import com.sardinecorp.blissapplication.ui.NoInternetFragment;
 import com.sardinecorp.blissapplication.utils.ConnectivityStatus;
 
 import butterknife.BindView;
@@ -17,8 +20,10 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.toolbar_main)
-    Toolbar mMainToolbar;
+    @BindView(R.id.toolbar_main) Toolbar mMainToolbar;
+    @BindView(R.id.fragment_holder) FrameLayout mFragmentHolder;
+
+    public static final String FRAGMENT_CONNECTIVITY_TAG = "FRAGMENT_CONNECTIVITY_TAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +41,32 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onReceive(Context context, Intent intent) {
             if(ConnectivityStatus.isConnected(getApplicationContext())) {
+                // TODO: Delete me!
                 Toast.makeText(context, "We have internet", Toast.LENGTH_SHORT).show();
+                removeNoConnectionFragment();
             } else {
+                // TODO: Delete me!
                 Toast.makeText(context, "We don't have internet", Toast.LENGTH_SHORT).show();
+                addNoConnectionFragment();
             }
         }
     };
+
+    private void removeNoConnectionFragment() {
+        Fragment fragment = getSupportFragmentManager().findFragmentByTag(FRAGMENT_CONNECTIVITY_TAG);
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+                    .remove(fragment)
+                    .commit();
+        }
+    }
+
+    private void addNoConnectionFragment() {
+        NoInternetFragment fragment = new NoInternetFragment();
+        getSupportFragmentManager().beginTransaction()
+                .setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+                .add(R.id.fragment_holder, fragment, FRAGMENT_CONNECTIVITY_TAG)
+                .commit();
+    }
 }
