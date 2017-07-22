453e50b3ba83472800d813482086466350382e61
Vanethos <solid.goncalo@gmail.com>
2017-07-22 19:16:33 +0100

    Feature: Connectivity Status - Added a broadcast receiver to check internet status

--- app/src/main/java/com/sardinecorp/blissapplication/MainActivity.java
+++ app/src/main/java/com/sardinecorp/blissapplication/MainActivity.java
@@ -1,8 +1,16 @@
 package com.sardinecorp.blissapplication;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
+
+import com.sardinecorp.blissapplication.utils.ConnectivityStatus;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,5 +27,19 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         setSupportActionBar(mMainToolbar);
         mMainToolbar.setTitle("Bliss Application");
+        // register the receiver to check internet connection
+        this.registerReceiver(mBroadcastReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
     }
+    
+    // Create a receiver to check the internet connection status
+    private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if(ConnectivityStatus.isConnected(getApplicationContext())) {
+                Toast.makeText(context, "We have internet", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(context, "We don't have internet", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
 }
